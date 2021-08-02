9b3f401b8f625f60a534d6679b89f152ce6be4a1
Shray Swarup <shrayswarup@gmail.com>
2021-08-02 03:01:25 -0700

    CV2 Basis and pytess broken code

---
+++ dodger.py
@@ -0,0 +1,20 @@
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import cv2
+import pytesseract
+
+print('test')
+
+img = cv2.imread('img1.png')
+print(img.shape)                    # Full Screen: 1080 1920
+img_cv = img[59:490,832:1080]   # Tab Menu: X: 
+
+# Display Image
+#cv2.imshow('Image', imgCropped)
+#cv2.waitKey(0)
+
+img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+print(pytesseract.image_to_string(img_rgb))

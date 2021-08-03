a88bf53048c01956d9358f748c5936cae7b35c24
Shray Swarup <shrayswarup@gmail.com>
2021-08-02 19:18:39 -0700

    Calculations
    Need cropping and reader fix,
    line 30 authentication fail "Bite"

--- dodger.py
+++ dodger.py
@@ -6,12 +6,52 @@ except ImportError:
 import cv2
 import pytesseract
 import numpy as np
+import requests
+from requests.api import request
+
+key = '324ddd0c-2350-435d-9610-eb4fd6f1ec9d'
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+    def getUUID(self):
+        out = str(requests.get("https://api.mojang.com/users/profiles/minecraft/" + self.name).content)
+        if len(str(out)) == 3:
+            return '0'
+        else:
+            return out[out.find("id\":") + 5:out.find("\"", out.find("id\":") + 5)]
+        
+    def getFKDR(self):
+        uuid = self.getUUID()
+        if uuid == '0':
+            return "\t"
+        pData = requests.get('https://api.hypixel.net/player?uuid=' + uuid + '&key=' + key).json()
+        #print(pData['success'])
+        if pData['success'] and pData["player"] and pData["player"]["stats"] and pData["player"]["stats"]["Bedwars"]:
+            deaths = int(pData["player"]["stats"]["Bedwars"]["final_deaths_bedwars"])
+            kills = int(pData["player"]["stats"]["Bedwars"]["final_kills_bedwars"])
+            # fours specific stat - four_four_final_deaths_bedwars
+            return round(kills/deaths, 2)
+        else:
+            return -1
+    def toString(self):
+        # numTabs = 6 - (len(self.name) - 6) // 8
+        numTabs = 4
+        if len(self.name) > 9:
+            numTabs = 3
+        if len(self.name) > 17:
+            numTabs = 2
+        #numTabs = 6 
+        tabs = '\t' * numTabs
+        return 'NAME: ' + self.name + tabs + 'FKDR: ' + str(self.getFKDR()) + '\t\tUUID: ' + self.getUUID()
+
+# PULL IMAGE
 
 img = cv2.imread('img1.png')
 
 # TRANSFORM IMAGE
 
-img_cv = img[59:490,832:1080]   # Crop to Tab Menu
+img_cv = img[59:490,832:1080]   # Crop to Tab Menu 1/3:832 4:815  1:9/16, 3: 6/16, 4: 9/16
 img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)  # Convert to Grayscale
 alpha = 3       # Contrast Multiplier
 beta = -200     # Brightness Addition
@@ -19,17 +59,21 @@ final = cv2.addWeighted(img_rgb, alpha, np.zeros(img_rgb.shape, img_rgb.dtype), 
 
 # MANIPULATE STRINGS
 
-print(pytesseract.image_to_string(final))
-names = [y for y in (x.strip() for x in pytesseract.image_to_string(final).splitlines()) if y]
-for x in range(len(names)):
-    if names[x] == '0':
-        print('test')
-print(names)
-#Remove spaces, anything with illegal characters, and copyright symbol
+#print(pytesseract.image_to_string(final))
+players = [Player(y) for y in (x.strip() for x in pytesseract.image_to_string(final).splitlines()) if y]
+for x in range(len(players)):
+    players[x].name = players[x].name.replace('@','0')    # 0 @ confusion fix
+    players[x].name = players[x].name.translate({ord(c): None for c in ' ©?()[]!-—=+'})    # Remove blacklisted chars
+    print(players[x].name)
+
+print()
+for x in range(len(players)):
+    print(players[x].toString())
+
 
 # DISPLAY IMAGE
 
-cv2.imshow('Image', final)
-cv2.waitKey(0)
+#cv2.imshow('Image', final)
+#cv2.waitKey(0)
 
 
--- test.py
+++ test.py
@@ -1,4 +1,22 @@
 
 print('=======')
-text = 'test\ntest2'
-print(text)
+import requests
+from requests.api import request
+username = "Shrasfdsgd"
+out = str(requests.get("https://api.mojang.com/users/profiles/minecraft/" + username).content)
+print('out' + out)
+if len(str(out)) == 3:
+    print('yeet')
+else:
+    uuid = out[out.find("id\":") + 5:out.find("\"", out.find("id\":") + 5)]
+
+    key = '324ddd0c-2350-435d-9610-eb4fd6f1ec9d'
+
+    player = requests.get('https://api.hypixel.net/player?uuid=' + uuid + '&key=' + key).json()
+    deaths = int(player["player"]["stats"]["Bedwars"]["final_deaths_bedwars"])
+    kills = int(player["player"]["stats"]["Bedwars"]["final_kills_bedwars"])
+    print(round(kills/deaths, 2))
+#324ddd0c-2350-435d-9610-eb4fd6f1ec9d
+#final_deaths_bedwars
+#final_kills_bedwars
+#four_four_final_deaths_bedwars

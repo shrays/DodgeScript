fdf2f6fb9af86377b4d32ad7f8f3c72af70d5ef6
Shray Swarup <shrayswarup@gmail.com>
2021-08-02 19:50:22 -0700

    fixes

--- dodger.py
+++ dodger.py
@@ -26,32 +26,30 @@ class Player:
         if uuid == '0':
             return "\t"
         pData = requests.get('https://api.hypixel.net/player?uuid=' + uuid + '&key=' + key).json()
-        #print(pData['success'])
         if pData['success'] and pData["player"] and pData["player"]["stats"] and pData["player"]["stats"]["Bedwars"]:
             deaths = int(pData["player"]["stats"]["Bedwars"]["final_deaths_bedwars"])
             kills = int(pData["player"]["stats"]["Bedwars"]["final_kills_bedwars"])
             # fours specific stat - four_four_final_deaths_bedwars
             return round(kills/deaths, 2)
         else:
-            return -1
+            return "\t"
     def toString(self):
-        # numTabs = 6 - (len(self.name) - 6) // 8
         numTabs = 4
         if len(self.name) > 9:
             numTabs = 3
         if len(self.name) > 17:
             numTabs = 2
-        #numTabs = 6 
         tabs = '\t' * numTabs
         return 'NAME: ' + self.name + tabs + 'FKDR: ' + str(self.getFKDR()) + '\t\tUUID: ' + self.getUUID()
 
 # PULL IMAGE
 
-img = cv2.imread('img1.png')
+
+img = cv2.imread('img4.png')
 
 # TRANSFORM IMAGE
 
-img_cv = img[59:490,832:1080]   # Crop to Tab Menu 1/3:832 4:815  1:9/16, 3: 6/16, 4: 9/16
+img_cv = img[59:490,815:1080]   # Crop to Tab Menu 1/3:832 4:815  1:9/16, 3: 6/16, 4: 9/16
 img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)  # Convert to Grayscale
 alpha = 3       # Contrast Multiplier
 beta = -200     # Brightness Addition
@@ -64,12 +62,15 @@ players = [Player(y) for y in (x.strip() for x in pytesseract.image_to_string(fi
 for x in range(len(players)):
     players[x].name = players[x].name.replace('@','0')    # 0 @ confusion fix
     players[x].name = players[x].name.translate({ord(c): None for c in ' ©?()[]!-—=+'})    # Remove blacklisted chars
-    print(players[x].name)
+    if players[x].name.startswith('0'):
+        players[x].name = players[x].name[1:]
+    #print(players[x].name)
+
+# Print Data
 
 print()
 for x in range(len(players)):
     print(players[x].toString())
-
 
 # DISPLAY IMAGE
 

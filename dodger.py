81192cec05305ae56675e3343ea2c72d76f66e24
Shray Swarup <shrayswarup@gmail.com>
2021-08-04 01:42:42 -0700

    Comments

--- dodger.py
+++ dodger.py
@@ -13,20 +13,20 @@ import shutil
 import time
 import statistics
 
-key = '324ddd0c-2350-435d-9610-eb4fd6f1ec9d'
+key = '324ddd0c-2350-435d-9610-eb4fd6f1ec9d'    # Hypixel in game /api key
 path = '/Users/shrayswarup/Library/Application Support/minecraft/screenshots/'
 
 class Player:
     def __init__(self, name):
         self.name = name
-    def getUUID(self):
+    def getUUID(self): # Read username to UUID via Mojang
         out = str(requests.get("https://api.mojang.com/users/profiles/minecraft/" + self.name).content)
-        if len(str(out)) == 3:
+        if len(str(out)) == 3: # 3 character - invalid user, empty string
             return '0'
         else:
             return out[out.find("id\":") + 5:out.find("\"", out.find("id\":") + 5)]
         
-    def getFKDR(self):
+    def getFKDR(self): # UUID to stats via Hypixel API
         uuid = self.getUUID()
         if uuid == '0':
             return "\t"
@@ -39,7 +39,8 @@ class Player:
             return round(kills/deaths, 2)
         except:
             return "\t"
-    def toString(self):
+
+    def toString(self): # Prints player and stats
         numTabs = 4
         if len(self.name) > 9:
             numTabs = 3
@@ -47,15 +48,21 @@ class Player:
             numTabs = 2
         tabs = '\t' * numTabs
         return 'NAME: ' + self.name + tabs + 'FKDR: ' + str(self.getFKDR()) + '\t\tUUID: ' + self.getUUID()
-def imageCrop(img):
-    # CONVERT TO GRAYSCALE, ADD CONTRAST
+
+def imageCrop(img): # Detects width of tab list and crops
+    # PIXEL CROPPING
+
+    # Screen Resolution - 1920 x 1080
+    # Head logo Width - 24 pixels
+    # Wifi logo Width - 33 pixels
+    # Brightness check within bounds of smallest X: 785 and largest X: 1135
+
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Convert to Grayscale
-    con1 = 20       # Contrast Multiplier
+    con1 = 20           # Contrast Multiplier
     bright1 = -1000     # Brightness Addition
-    final = cv2.addWeighted(img_rgb, con1, np.zeros(img_rgb.shape, img_rgb.dtype), 0, bright1)
+    final = cv2.addWeighted(img_rgb, con1, np.zeros(img_rgb.shape, img_rgb.dtype), 0, bright1) # Add contrast/brightness
 
     # 2D LIST, PIXEL COLORS
-    print(type(int(final[65,1135])))
     color = []
     for i in range(355):
         temp = []
@@ -80,71 +87,43 @@ def imageCrop(img):
     crop = img[59:490,960 - (crop - 960) + 24:crop - 33]   # Crop to Tab Menu 
     return crop
 
-def imageRead(crop):
-    # TRANSFORM IMAGE
-
-    #img_cv = img[59:490,832:1080]   # Crop to Tab Menu 1/3:832 4:815  1:9/16, 3: 6/16, 4: 9/16
+def imageRead(crop):    # Image to text
     img_rgb = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)  # Convert to Grayscale
     alpha = 3       # Contrast Multiplier
     beta = -200     # Brightness Addition
     final = cv2.addWeighted(img_rgb, alpha, np.zeros(img_rgb.shape, img_rgb.dtype), 0, beta)
 
     # DISPLAY IMAGE - STOPS PROGRAM
-
     #cv2.imshow('Image', final)
     #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-
     return final
 
-def text(final):
-    # MANIPULATE STRINGS
-
+def text(final): # Cleans and sorts text
     #print('RAW ===========================================')
     #print(pytesseract.image_to_string(final))
     #print('POST ==========================================')
-
     players = [Player(y) for y in (x.strip() for x in pytesseract.image_to_string(final).splitlines()) if y]
     for x in range(len(players)):
-        players[x].name = players[x].name.replace('@','0')    # 0 @ confusion fix
+        players[x].name = players[x].name.replace('@','0')     #@ to 0 confusion fix
         players[x].name = players[x].name.translate({ord(c): None for c in ' .©?()[]!-—=+'})    # Remove blacklisted chars
-        if players[x].name.startswith('0'):
+        if players[x].name.startswith('0'): # Lunar client symbol fix
             players[x].name = players[x].name[1:]
         #print(players[x].name)
 
     # Print Data
-
     for x in range(len(players)):
         print(players[x].toString())
 
-    shutil.move(path + f, path + "UsedDodger/")
+    shutil.move(path + f, path + "UsedDodger/") # Moves image away from SS folder (UsedDodger file in SS)
 
-# PULL IMAGE
+# PULL TEST IMAGE
 #img = cv2.imread('img1.png')
 
-while True:
-    files = os.listdir(path)
+while True: # Always Runs
+    files = os.listdir(path) # Checks screenshots folders (.DS(mac), UsedDodger Folder, and Regular SS )
     if len(files) > 3:
         time.sleep(1) # Lets image load into 
         for f in files:
-            #print(f)
             if f[0] == '2':
-                #print('starts with 2')
                 img = cv2.imread(os.path.expanduser(path + f))
                 text(imageRead(imageCrop(img)))
-                #shutil.move(path + f, path + "UsedDodger/")
-
-
-# PIXEL CROPPING
-
-# 1920 x 1080
-# Half X = 960
-
-# RIGHT MOST LIGHT TO DARK TRANSITION
-# X = 1055 1130
-# LEFT MOST (IN LIGHT)
-# X =  868  789
-
-# Y = 59/60 - 488/499
-# Head Width - 24 pixels
-# Wifi Width - 33 pixels
